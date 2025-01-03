9be3f4ad53454824478d251f855c86197cc28f88
Gerson <gersonherrerach@gmail.com>
2025-01-03 11:50:36 -0500

    v.3

--- noemi_store/settings.py
+++ noemi_store/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$g%bu(clatbf(_mp@ab-i6((-*dhb9&dmi4u$5t^lsri5&i28_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG' ,cast=bool )
 
 
 ALLOWED_HOSTS = []
